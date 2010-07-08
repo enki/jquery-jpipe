1934947c2d37b79384c4c4634c7dfb1e3712f9fa
Paul Bohm <enki@bbq.io>
2010-07-07 18:31:05 -0700

    switch to different logger

--- jquery.jpipe.js
+++ jquery.jpipe.js
@@ -1,7 +1,4 @@
 (function($) { // hide the namespace
-    // make sure logging doesn't raise
-    try { console = console || {};} catch (e) {}
-    try { console.log = console.log || function(what){}; } catch (e) {}
 
     $.jpipe = {
         version: '0.1',
@@ -20,37 +17,37 @@
             args.url = '/queue/';
             args.patience = 6000;
             
-            console.log( $.format("Subscribing to {queue}.", args) );
+            $.log( $.format("Subscribing to {queue}.", args) );
             if (this.subscriptions[args.queue]) {
-                console.log('already subscribed');
+                $.log('already subscribed');
             } else {
                 this.subscriptions[args.queue] = args;
             }
         },
         unsubscribe: function(args) {
-            console.log( $.format("Unsubscribing from {queue}.", args) );
+            $.log( $.format("Unsubscribing from {queue}.", args) );
             this.subscriptions[args.queue] = null;
         },
         publish: function(args) {
-            console.log( $.format("Subscribing to {queue}.", args) );
+            $.log( $.format("Subscribing to {queue}.", args) );
             // queue = this.subscriptions[args.queue]; // not needed
         },
         xhrcallback: function(data) {
             localqueue = $.jpipe.subscriptions[data.queue.queue];
             if (data.queue.callcnt < localqueue.callcnt) {
-                console.log('old xhr' + data.queue.callcnt + '/' + localqueue.callcnt + 
+                $.log('old xhr' + data.queue.callcnt + '/' + localqueue.callcnt + 
                             'returned.');
-                // console.log('ignoring.');
+                // $.log('ignoring.');
                 // return;
             }
-            console.log('xhr' + data.queue.callcnt + '/' + localqueue.callcnt + ' returned');
+            $.log('xhr' + data.queue.callcnt + '/' + localqueue.callcnt + ' returned');
             var d = new Date();
             var now = d.valueOf();
             localqueue.last_response_time = now;
             
             $.jpipe.poll(localqueue);
             
-            console.log(data);
+            $.log(data);
             
             if (data.queue.message) {
                 localqueue.cb(data.queue.message);
@@ -58,7 +55,7 @@
         },
         poll: function(args) {
             args.callcnt += 1;
-            console.log('polling ' + args.callcnt);
+            $.log('polling ' + args.callcnt);
             var url = $.format('{url}?callback=?&data={params}', {
                 url: args.url,
                 params: $.json.encode({
@@ -90,9 +87,9 @@
                 var d = new Date();
                 var now = d.valueOf();
                 var age = now - queue.last_response_time;
-                // console.log(age);
+                // $.log(age);
                 if (age > queue.patience) {
-                    console.log('impatient! ' + age);
+                    $.log('impatient! ' + age);
                     $.jpipe.poll(queue);
                 }
             });
